52ad2ff46435d1d4ca2a52c49ad85c5f05cfa553
DH <adlandh@gmail.com>
2025-12-31 22:05:46 +0100

    Update response dumper behavior and tests

--- resp_dumper.go
+++ resp_dumper.go
@@ -13,8 +13,11 @@ import (
 type Dumper struct {
 	http.ResponseWriter
 
-	mw  io.Writer
-	buf *bytes.Buffer
+	mw           io.Writer
+	buf          *bytes.Buffer
+	statusCode   int
+	wroteHeader  bool
+	bytesWritten int
 }
 
 func NewDumper(respWriter http.ResponseWriter) *Dumper {
@@ -29,10 +32,16 @@ func NewDumper(respWriter http.ResponseWriter) *Dumper {
 }
 
 func (d *Dumper) Write(b []byte) (int, error) {
+	if !d.wroteHeader {
+		d.WriteHeader(http.StatusOK)
+	}
+
 	nBytes, err := d.mw.Write(b)
 	if err != nil {
 		err = fmt.Errorf("error writing response: %w", err)
 	}
+
+	d.bytesWritten += nBytes
 
 	return nBytes, err
 }
@@ -41,9 +50,73 @@ func (d *Dumper) GetResponse() string {
 	return d.buf.String()
 }
 
+func (d *Dumper) StatusCode() int {
+	if d.wroteHeader {
+		return d.statusCode
+	}
+
+	return http.StatusOK
+}
+
+func (d *Dumper) BytesWritten() int {
+	return d.bytesWritten
+}
+
+func (d *Dumper) WriteHeader(statusCode int) {
+	if d.wroteHeader {
+		return
+	}
+
+	d.statusCode = statusCode
+	d.wroteHeader = true
+	d.ResponseWriter.WriteHeader(statusCode)
+}
+
 func (d *Dumper) Flush() {
 	if flusher, ok := d.ResponseWriter.(http.Flusher); ok {
 		flusher.Flush()
+	}
+}
+
+func (d *Dumper) Push(target string, opts *http.PushOptions) error {
+	if pusher, ok := d.ResponseWriter.(http.Pusher); ok {
+		if err := pusher.Push(target, opts); err != nil {
+			return fmt.Errorf("error pushing resource: %w", err)
+		}
+
+		return nil
+	}
+
+	return http.ErrNotSupported
+}
+
+func (d *Dumper) ReadFrom(r io.Reader) (int64, error) {
+	buf := make([]byte, 32*1024)
+
+	var total int64
+
+	for {
+		n, err := r.Read(buf)
+		if n > 0 {
+			written, writeErr := d.Write(buf[:n])
+			total += int64(written)
+
+			if writeErr != nil {
+				return total, writeErr
+			}
+
+			if written < n {
+				return total, io.ErrShortWrite
+			}
+		}
+
+		if err != nil {
+			if err == io.EOF {
+				return total, nil
+			}
+
+			return total, fmt.Errorf("error reading response source: %w", err)
+		}
 	}
 }
 
@@ -57,5 +130,5 @@ func (d *Dumper) Hijack() (net.Conn, *bufio.ReadWriter, error) {
 		return conn, rw, err
 	}
 
-	return nil, nil, nil
+	return nil, nil, http.ErrNotSupported
 }
